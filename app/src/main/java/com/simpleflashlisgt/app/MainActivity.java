3df2fbecbe123276de30293fb82b59339ab9cc83
sambulosenda <sambulosendas@gmail.com>
2013-12-30 22:56:45 +0000

    Initial commit

---
+++ app/src/main/java/com/simpleflashlisgt/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.simpleflashlisgt.app;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/simpleflashlisgt/app/WidgetPovider.java
@@ -0,0 +1,11 @@
+package com.simpleflashlisgt.app;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by sambulosenda on 30/12/13.
+ */
+public class WidgetPovider  extends AppWidgetProvider{
+
+
+}
